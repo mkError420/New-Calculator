da9e00fd3ba8dc54420afd5199774dbba3c25bef
mkError420 <mk.rabbani.cse@gmail.com>
2024-11-12 03:35:50 +0600

    Initial commit

--- src/Components/ButtonContainer.jsx
+++ src/Components/ButtonContainer.jsx
@@ -2,7 +2,7 @@ import styles from "./ButtonContainer.module.css"
 const ButtonContainer =({onButtonClick})=>{
 
   const buttonName = [
-    'C','(',')','/','7','8','9','*','4','5','6','-','1','2','3','+','~','0','.','=',
+    'C','1','2','3','4','5','6','7','8','9','(',')','/','*','-','+','~','0','.','=',
   ]
 
 return<div className={styles.buttonsContainer}>
